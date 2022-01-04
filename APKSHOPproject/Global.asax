9afa3764f5487abad9b98a3d88b56769531f12fb
MaamounMorgan <maamounmorgan@gmail.com>
2022-01-04 13:00:12 +0200

    xxxx

---
+++ APKSHOPproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APKSHOPproject.MvcApplication" Language="C#" %>
---
+++ APKSHOPproject/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AKShopDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }  
+        public virtual Category Category { get; set; }
+
+        public int BrandId { get; set; }
+        public virtual Brand Brand { get; set; }
+    }
+}
